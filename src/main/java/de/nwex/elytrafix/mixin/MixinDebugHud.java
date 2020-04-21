321d341e72d48803fb8c99c184000a0fbda0db82
Jakob-Niklas See <git@nwex.de>
2020-04-21 23:22:51 +0200

    Added debug screen info

---
+++ src/main/java/de/nwex/elytrafix/mixin/MixinDebugHud.java
@@ -0,0 +1,24 @@
+package de.nwex.elytrafix.mixin;
+
+import de.nwex.elytrafix.ElytraFix;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(DebugHud.class)
+public class MixinDebugHud
+{
+    @Inject(method = "getLeftText", at = @At("RETURN"))
+    private void getLeftText(CallbackInfoReturnable<List<String>> ci)
+    {
+        List<String> leftText = ci.getReturnValue();
+
+        leftText.add((ElytraFix.active ? "ElytraFix active " : "ElytraFix inactive ") + (ElytraFix.shouldElytraFly ? "(fallFlying)" : "(onGround)"));
+
+        ci.setReturnValue(leftText);
+    }
+}
